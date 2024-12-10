838119df79acbfec569f60edc2d4cc0833c9d02a
Vladimir Rudkovskiy <rudkovskyvladimir21051@gmail.com>
2024-12-10 13:58:30 +0200

    implimenting About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Hero from './components/Hero'
+import About from './components/About'
 
 const App = () => {
 	return (
 		<main className='relative mon-h-screen w-screen overflow-x-hidden'>
 			<Hero />
 
-			<section className='z-0 min-h-screen bg-blue-500'/>
+			<About />
 		</main>
 	)
 }
