ec497646f938254a85e0ee5561f7ff1885b709f7
Vladimir Rudkovskiy <rudkovskyvladimir21051@gmail.com>
2024-12-10 15:21:38 +0200

    implimenting navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Hero from './components/Hero'
 import About from './components/About'
+import Navbar from './components/Navbar'
 
 const App = () => {
 	return (
 		<main className='relative mon-h-screen w-screen overflow-x-hidden'>
+			<Navbar />
 			<Hero />
-
 			<About />
 		</main>
 	)
